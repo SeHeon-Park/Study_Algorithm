5420e1005d834caa9018e4022b64575b4edf4650
seheon <quwe153@naver.com>
2022-06-17 00:17:54 +0900

    programmers

--- 프로그래머스/[1차] 프렌즈4블록/Main.py
+++ 프로그래머스/[1차] 프렌즈4블록/Main.py
@@ -1,9 +1,71 @@
-def gcd(w, h):
-    for i in range(min(w, h), 0, -1):
-        if w%i==0 and h%i==0:
-            return i
+from collections import deque
 
-def solution(w,h):
-    g = gcd(w, h)
-    answer = w*h-int((w/g+h/g-1)*g)
+
+def pop_item(board, visited):
+    boardV2 = [[] for _ in range(len(board))]
+    visitedV2 = [[] for _ in range(len(board))]
+    cnt = 0
+    for i in range(len(board)):
+        deq1 = deque(board[i])
+        deq2 = deque(visited[i])
+        while deq1:
+            m = deq2.popleft()
+            if m == 0:
+                deq1.popleft()
+                cnt += 1
+            else:
+                visitedV2[i].append(m)
+                boardV2[i].append(deq1.popleft())
+    return boardV2, visitedV2, cnt
+
+
+def visit(visited, x_l, y_l):
+    x = [1, 0, 1]
+    y = [0, 1, 1]
+    visited[y_l][x_l] = 0
+    x_temp = x_l
+    y_temp = y_l
+    for i in zip(x, y):
+        x_l = x_temp + i[0]
+        y_l = y_temp + i[1]
+        visited[y_l][x_l] = 0
+    return visited
+
+
+def select(board, x_l, y_l, visited):
+    x = [1, 0, 1]
+    y = [0, 1, 1]
+    x_temp = x_l
+    y_temp = y_l
+    flag = 0
+    for i in zip(x, y):
+        x_l = x_temp + i[0]
+        y_l = y_temp + i[1]
+        if board[y_temp][x_temp] != board[y_l][x_l]:
+            flag = 1
+            break
+    if flag == 0:
+        visited = visit(visited, x_temp, y_temp)
+    return visited
+
+
+def solution(m, n, board):
+    boardV2 = []
+    answer = 0
+    cnt = 0
+    visited = [[1 for _ in range(m)] for _ in range(n)]
+    for r in range(n):
+        part = []
+        for h in range(m - 1, -1, -1):
+            part.append(board[h][r])
+        boardV2.append(part)
+    while True:
+        for j in range(n - 1):
+            m = min(len(boardV2[j]), len(boardV2[j+1]))
+            for i in range(m-1):
+                visited = select(boardV2, i, j, visited)
+        boardV2, visited, cnt = pop_item(boardV2, visited)
+        if cnt == 0:
+            break
+        answer += cnt
     return answer
--- 프로그래머스/[1차]뉴스 클러스터링/Main.py
+++ 프로그래머스/[1차]뉴스 클러스터링/Main.py
@@ -1,71 +1,26 @@
-from collections import deque
+def inter_union(s1, s2):
+    inter, union = [], [] # 교집합, 합집합
+    for i in s1:
+        for j in s2:
+            if i == j:
+                inter.append(i)
+                s2.remove(j)
+                break
+        union.append(i)
+    for i in s2:
+        union.append(i)
+    return inter, union
 
-
-def pop_item(board, visited):
-    boardV2 = [[] for _ in range(len(board))]
-    visitedV2 = [[] for _ in range(len(board))]
-    cnt = 0
-    for i in range(len(board)):
-        deq1 = deque(board[i])
-        deq2 = deque(visited[i])
-        while deq1:
-            m = deq2.popleft()
-            if m == 0:
-                deq1.popleft()
-                cnt += 1
-            else:
-                visitedV2[i].append(m)
-                boardV2[i].append(deq1.popleft())
-    return boardV2, visitedV2, cnt
-
-
-def visit(visited, x_l, y_l):
-    x = [1, 0, 1]
-    y = [0, 1, 1]
-    visited[y_l][x_l] = 0
-    x_temp = x_l
-    y_temp = y_l
-    for i in zip(x, y):
-        x_l = x_temp + i[0]
-        y_l = y_temp + i[1]
-        visited[y_l][x_l] = 0
-    return visited
-
-
-def select(board, x_l, y_l, visited):
-    x = [1, 0, 1]
-    y = [0, 1, 1]
-    x_temp = x_l
-    y_temp = y_l
-    flag = 0
-    for i in zip(x, y):
-        x_l = x_temp + i[0]
-        y_l = y_temp + i[1]
-        if board[y_temp][x_temp] != board[y_l][x_l]:
-            flag = 1
-            break
-    if flag == 0:
-        visited = visit(visited, x_temp, y_temp)
-    return visited
-
-
-def solution(m, n, board):
-    boardV2 = []
-    answer = 0
-    cnt = 0
-    visited = [[1 for _ in range(m)] for _ in range(n)]
-    for r in range(n):
-        part = []
-        for h in range(m - 1, -1, -1):
-            part.append(board[h][r])
-        boardV2.append(part)
-    while True:
-        for j in range(n - 1):
-            m = min(len(boardV2[j]), len(boardV2[j+1]))
-            for i in range(m-1):
-                visited = select(boardV2, i, j, visited)
-        boardV2, visited, cnt = pop_item(boardV2, visited)
-        if cnt == 0:
-            break
-        answer += cnt
-    return answer
+def solution(str1, str2):
+    str1 = [str1[i:i+2].upper() for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
+    str2 = [str2[i:i+2].upper() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
+    print(str1, str2)
+    if len(str1) == 0 and len(str2) == 0:
+        return 65536
+    if len(str1) > len(str2):
+        inter, union = inter_union(str1, str2)
+    else:
+        inter, union = inter_union(str1, str2)
+    if len(inter) == 0:
+        return 0
+    return int((len(inter)/len(union)) * 65536)
