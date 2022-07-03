66829141ece381b79b26a42afda65d7738fe260e
seheon <quwe153@naver.com>
2022-07-03 22:08:01 +0900

    programmers: wq qwd wq

--- 프로그래머스/단어변환/Main.py
+++ 프로그래머스/단어변환/Main.py
@@ -1,33 +1,29 @@
-def check(curr,  next):
-    count = 0
-    for i in range(len(curr)):
-        if curr[i] != next[i]:
-            count += 1
-    if count == 1:
-        return 1
-    else:
-        return -1
+def check(s, n):
+    index = 0
+    cnt = 0
+    while index != len(s):
+        if cnt > 1:
+            return False
+        if s[index] != n[index]:
+            cnt += 1
+        index += 1
+    if cnt == 1:
+        return True
+    return False
 
-def visit(begin, target, words, count, visited):
-    if begin == target:
-        answer.append(count)
+def recursive(curr, target, words, cnt):
+    if curr == target:
+        answer.append(cnt)
         return
     for i in range(len(words)):
-        t = check(begin, words[i])
-        if t == 1 and visited[i] == 0:
-            visited[i] = 1
-            count += 1
-            visit(words[i], target, words, count, visited)
-            count -= 1
-            visited[i] = 0
+        if check(curr, words[i]):
+            recursive(words[i], target, words[:i]+words[i+1:], cnt+1)
     return
 
-
 def solution(begin, target, words):
-    visited = [0 for _ in range(len(words))]
-    visit(begin, target, words, 0, visited)
-    if len(answer) == 0:
+    global answer
+    answer = []
+    if not target in words:
         return 0
+    recursive(begin, target, words, 0)
     return min(answer)
-
-answer = []
