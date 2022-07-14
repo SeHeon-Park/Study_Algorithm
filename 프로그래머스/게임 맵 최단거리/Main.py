d1ae9d62d888875798894a9b68e2036b89893f8f
seheon <quwe153@naver.com>
2022-07-14 14:57:11 +0900

    programmers

---
+++ 프로그래머스/게임 맵 최단거리/Main.py
@@ -0,0 +1,30 @@
+import math
+from collections import deque
+
+def solution(maps):
+    visit = [[0 for _ in range(len(maps[0]))] for _ in range(len(maps))]
+    Q = deque([(0, 0, 1)]) # x, y, cnt
+    ans = math.inf
+    dx = [1, 0, 0, -1]
+    dy = [0, 1, -1, 0]
+    while Q:
+        info = Q.popleft()
+        x, y, cnt = info[0], info[1], info[2]
+        if x == len(maps[0])-1 and y == len(maps)-1:
+            ans = min(cnt, ans)
+            continue
+        for sx, sy in zip(dx, dy):
+            x += sx
+            y += sy
+            if 0<=x<len(maps[0]) and 0<=y<len(maps) and visit[y][x]==0 and maps[y][x]==1:
+                visit[y][x] = 1
+                Q.append((x, y, cnt+1))
+            x -= sx
+            y -= sy
+    if ans == math.inf:
+        return -1
+    return ans
+
+주의!
+- dfs를 이용하면 효율성 통과x
+- bfs를 이용해서 해야됨(queue이용)
