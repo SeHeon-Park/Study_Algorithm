6e84357c8857baf7433801f316f06e6c9630984c
SeHeon-Park <68144687+SeHeon-Park@users.noreply.github.com>
2022-10-21 14:14:29 +0900

    Create 1208. [SW 문제해결 기본] 1일차 - Flatten.py

---
+++ SW Expert Academy/1208. [SW 문제해결 기본] 1일차 - Flatten.py
@@ -0,0 +1,24 @@
+import heapq
+ 
+for t in range(10):
+    n = int(input())
+    MIN = [int(x) for x in input().split()]
+    MAX = []
+    flag = 0
+    for m in MIN:
+        MAX.append(-m)
+    heapq.heapify(MIN)
+    heapq.heapify(MAX)
+    for _ in range(n):
+        a = heapq.heappop(MAX)
+        b = heapq.heappop(MIN)
+        if -a-b <= 1:
+            flag = 1
+            break
+        heapq.heappush(MAX, a + 1)
+        heapq.heappush(MIN, b + 1)
+ 
+    if flag:
+        print("#{} {}".format(t + 1, -a - b))
+    else:
+        print("#{} {}".format(t + 1, -heapq.heappop(MAX) - heapq.heappop(MIN)))
