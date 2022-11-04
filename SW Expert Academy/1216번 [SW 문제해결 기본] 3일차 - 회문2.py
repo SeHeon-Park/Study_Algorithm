c9481cd69a315ed1f7a915118f68caff02e9a589
seheon <quwe153@naver.com>
2022-11-05 03:27:51 +0900

    완전 탐색

--- SW Expert Academy/1216번 [SW 문제해결 기본] 3일차 - 회문2.py
+++ SW Expert Academy/1216번 [SW 문제해결 기본] 3일차 - 회문2.py
@@ -23,20 +23,17 @@ for t in range(10):
     ans = -math.inf
     for _ in range(100):
         M.append(list(map(str, input())))
-
     N = [int(x+1) for x in range(99)]
     L = list(combinations(N, 2))
     L.sort(key=lambda x:x[1]-x[0], reverse=True)
     for i in range(100):
         for l, r in L:
             if check_w(l, r, i):
-                if ans != -math.inf and ans >= r-l+1:
-                    break
-                ans = r-l+1
+                ans = max(ans, r - l + 1)
+                break
             if check_h(l, r, i):
-                if ans != -math.inf and ans >= r-l+1:
-                    break
-                ans = r-l+1
+                ans = max(ans, r - l + 1)
+                break
     if ans != -math.inf:
         print("#{} {}".format(t+1, ans))
     else:
