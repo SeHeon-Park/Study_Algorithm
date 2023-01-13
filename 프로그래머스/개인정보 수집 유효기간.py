8268fd108534cbf25b426ff8f47a768133b18586
seheon <quwe153@naver.com>
2023-01-13 21:20:58 +0900

    날짜

---
+++ 프로그래머스/개인정보 수집 유효기간.py
@@ -0,0 +1,68 @@
+import re
+
+def expired(s, T):
+    info = re.split("[ ]", s)
+    v = info[1]  # 약관
+    time = T[v]
+    start = re.split("[.]", info[0])
+    y, m, d = int(start[0]), int(start[1]), int(start[2])
+    ## 달
+    m += time
+    if m > 12:
+        r = m // 12
+        m = m % 12
+        if m == 0:
+            m = 12
+            y -= 1
+        y += r
+    ## 일
+    d -= 1
+    if d == 0:
+        m -= 1
+        if m == 0:
+            m = 12
+            y -= 1
+        d = 28
+    return [y, m, d]
+
+def check(t, c):
+    if t[0] < c[0]:
+        return False
+    elif t[0] > c[0]:
+        return True
+    if t[1] < c[1]:
+        return False
+    elif t[1] > c[1]:
+        return True
+
+    if t[2] < c[2]:
+        return False
+    elif t[2] > c[2]:
+        return True
+    return False
+
+
+def solution(today, terms, privacies):
+    answer = []
+    target = []
+    for t in re.split("[.]", today):
+        target.append(int(t))
+
+    T = {}
+    for t in terms:
+        info = re.split("[ ]", t)
+        T[info[0]] = int(info[1])
+
+    idx = 1
+    for p in privacies:
+        if check(target, expired(p, T)):
+            answer.append(idx)
+        idx += 1
+
+    return answer
+
+
+
+
+print(solution("2022.02.28", ["A 23"], ["2020.01.28 A"]))
+
